1045c6a85b5ded49e55cbb802f653caaf16b8acb
David Lance <dlance1996@gmail.com>
2022-11-01 14:21:44 -0400

    Initial numberOfReport commit

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,12 +2,14 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
+
+import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -45,5 +47,46 @@ public class EmployeeServiceImpl implements EmployeeService {
         LOG.debug("Updating employee [{}]", employee);
 
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public ReportingStructure computeNumberOfReports(String id) {
+        LOG.debug("Computing number of reports for employee with id [{}]", id);
+
+        ReportingStructure returnReport = new ReportingStructure();
+        returnReport.setEmployeeId(id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+        List<Employee> directReports = employee.getDirectReports();
+        List<String> directReportIds = new ArrayList<>();
+
+        if (directReports == null) {
+            returnReport.setNumberOfReports(0);
+            return returnReport;
+        }
+
+        for (Employee directReport : directReports) {
+            directReportIds.add(directReport.getEmployeeId());
+        }
+
+        Set<String> totalReportIds = new HashSet<>(directReportIds);
+
+        for (int i = 0; i < directReportIds.size(); i++) {
+            Employee tempEmployee = employeeRepository.findByEmployeeId(directReportIds.get(i));
+            List<Employee> tempDirectReports = tempEmployee.getDirectReports();
+            int currentCount = totalReportIds.size();
+
+            for (int j = 0; tempDirectReports != null && j < tempDirectReports.size(); j++) {
+                totalReportIds.add(tempDirectReports.get(j).getEmployeeId());
+                // Handling case of an Employee reporting to multiple people
+                if (totalReportIds.size() > currentCount) {
+                    directReportIds.add(tempDirectReports.get(j).getEmployeeId());
+                }
+            }
+        }
+
+        returnReport.setNumberOfReports(totalReportIds.size());
+
+        return returnReport;
     }
 }
